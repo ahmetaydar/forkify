3bd2315478da4aeb3ea3284d9e41e1bf11388b7e
ahmetaydar <a.aydar2014@gmail.com>
2023-03-29 23:54:26 +0300

    pagination features added

--- src/js/controller.js
+++ src/js/controller.js
@@ -2,6 +2,7 @@ import * as model from './model.js';
 import recipeView from './views/recipeView.js';
 import searchView from './views/searchView.js';
 import resultsView from './views/resultsView.js';
+import paginationView from './views/paginationView';
 
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
@@ -43,6 +44,9 @@ const controlSearchResults = async function () {
 
     //    resultsView.render(model.state.search.results);
     resultsView.render(model.getSearchResultsPage());
+
+    // Render initial pagination buttons
+    paginationView.render(model.state.search);
   } catch (err) {
     console.log(err);
   }
