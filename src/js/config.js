df689929cb8e120bfd28e60704cd8cca7a8cb970
ahmetaydar <62577539+ahmetaydar@users.noreply.github.com>
2023-04-05 19:00:31 +0300

    Add files via upload

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '2da2f1ee-e2cf-4465-a2c1-6b82d7bb6bf0';
+export const MODAL_CLOSE_SEC = 2.5;
